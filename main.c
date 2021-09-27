9e8f721d328f8a691afbff2df0e0a40e03f8d9fc
sangcp <sangcpar@parksangchan-ui-MacBookAir.local>
2021-09-27 18:30:31 +0900

    cd 1

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "minishell.h"
 
-char		*get_path(char **envp)
+char		*get_env(char **envp, char *option)
 {
 	int		i;
 	int		j;
 	char	find[5];
 
 	i = 0;
-	ft_strlcpy(find, "PATH", 5);
+	ft_strlcpy(find, option, 5);
 	while (envp[i])
 	{
 		j = 0;
@@ -38,12 +38,27 @@ void get_cmd(char **cmd)
 	get_next_line(1, cmd);
 }
 
-int cmd_cd(char *cmd)
+char	*get_path(char **strs)
+{
+	if (!strs[1][1])
+		return ("");
+	else
+		return (strs[1] + 1);
+}
+
+int cmd_cd(char *cmd, char **envp)
 {
 	char **strs;
 
 	strs = ft_split(cmd, ' ');
-	chdir(strs[1]);
+	if (strs[1][0] == '~')
+	{
+		if ((chdir(ft_strjoin(get_env(envp, "HOME"), get_path(strs)))) == -1)
+			ft_putstr_fd("cd fail\n", 2);
+		return (0);
+	}
+	if ((chdir(strs[1])) == -1)
+		ft_putstr_fd("cd fail\n", 2);
 	return (0);
 }
 
@@ -107,15 +122,17 @@ int run_cmd(char *cmd, char **envp)
 	if (!(ft_strcmp(cmd, "exit")))
 		return (-1);
 	if (!(ft_strncmp(cmd, "cd", 2)))
-		return (cmd_cd(cmd));
+		return (cmd_cd(cmd, envp));
 	if (!(ft_strncmp(cmd, "echo", 4)))
 		return (cmd_echo(cmd + 5));
 	pid = fork();
 	i = 0;
 	if (pid == 0)
 	{
-		path = ft_split(get_path(envp), ':');
+		path = ft_split(get_env(envp, "PATH"), ':');
 		strs = ft_split(cmd, ' ');
+		if (!ft_strncmp(*strs, "/bin/", 5))
+			*strs = *strs + 5;
 		while (path[i])
 		{
 			tmp = ft_strjoin(path[i], "/");
